b21ea1c8451483b4f64d88025597416787cbc173
M Taimoor Ameen <106917553+TaimoorAmeen@users.noreply.github.com>
2025-08-14 04:22:33 -0700

    Create ClassComponent.jsx

---
+++ ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Student from './Student'
+const ClassComponent = () => {
+  return (
+    <div style={{backgroundColor: "skyblue", padding:"10px"}}>
+        <h1>ClassComponent</h1>
+         <Student/>
+    </div>
+  )
+}
+
+export default ClassComponent
