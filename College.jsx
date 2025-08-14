aad275016b49e08e41a5ed79a1cfecd6f8ae516d
M Taimoor Ameen <106917553+TaimoorAmeen@users.noreply.github.com>
2025-08-14 04:22:02 -0700

    Create College.jsx

---
+++ College.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClassComponent from './ClassComponent'
+
+const College = () => {
+  return (
+    <div style={{backgroundColor:"orange", padding:"10px"}}>
+        <h1>College Component</h1>
+        <ClassComponent/>
+    </div>
+  )
+}
+
+export default College
